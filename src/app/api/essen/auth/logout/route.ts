9084546e805efc2f2dc57fcef9b34ba91b68c336
matplo6 <160617215+matplo6@users.noreply.github.com>
2025-03-11 10:30:11 +0000

    change nextApiRequest to nextRequest In logout/route.ts

--- src/app/api/essen/auth/logout/route.ts
+++ src/app/api/essen/auth/logout/route.ts
@@ -1,14 +1,14 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { serialize } from "cookie";
+import { NextRequest, NextResponse } from "next";
+//import { serialize } from "cookie";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    res.setHeader("Set-Cookie", serialize("auth_token", "", {
+export async function POST(req: NextRequest, res: NextResponse) {
+    /*res.setHeader("Set-Cookie", serialize("auth_token", "", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "strict",
         path: "/",
         expires: new Date(0),
-    }));
+    }));*/
 
     return res.status(200).json({ message: "Logout erfolgreich" });
 }
